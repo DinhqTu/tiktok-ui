f4e1f88286cb68f4a12664d437ab1f081211d3a0
DinhTu <dinhqtu861@gmail.com>
2023-02-06 17:37:08 +0700

    Updates

--- src/assets/image/index.js
+++
@@ -1,8 +0,0 @@
-const image = {
-    logo: require('~/assets/image/logo.svg').default,
-    message: require('~/assets/image/message.svg').default,
-    inbox: require('~/assets/image/inbox.svg').default,
-    no_image: require('~/assets/image/noImage.png'),
-};
-
-export default image;
